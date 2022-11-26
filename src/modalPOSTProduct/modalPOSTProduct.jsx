90dc0667a1e36b76a61a3470a0798fbca9f5e6be
bebeloper <kevind171819@gmail.com>
2022-11-26 16:14:15 -0500

    first modal implementation

---
+++ src/modalPOSTProduct/modalPOSTProduct.jsx
@@ -0,0 +1,13 @@
+import './product.css'
+import React from 'react'
+
+/// change this line
+function Product({product}) {
+  return (
+    <div className='modal-background'>
+        Hola soy un modal
+    </div>
+  )
+}
+
+export { Product }
