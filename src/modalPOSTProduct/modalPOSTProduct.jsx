3221732703c8fc97b9b0d8aa9e982fc5f554e0f9
Kevind <profesionaldigital@impormedical.com.co>
2022-11-29 14:34:11 -0500

    modal finished

--- src/modalPOSTProduct/modalPOSTProduct.jsx
+++ src/modalPOSTProduct/modalPOSTProduct.jsx
@@ -1,60 +1,63 @@
 import './modalPOSTProduct.css'
 import React from 'react'
+import { useForm } from 'react-hook-form';
+
 import { MdCancel } from 'react-icons/md';
 import { RiSave3Fill } from 'react-icons/ri';
+import { useState } from 'react';
 
 /// change this line
 function ModalPostProduct({
   isOpen, openModal, setOpenModal, productsNameArray, setProductsName}) {
 
-  const closeModal = event =>{
+  const { register, formState: { errors }, handleSubmit, reset } = useForm();
+  const [ posting, setPosting] = useState(false);
+
+  const closeModal = (data) =>{
     setOpenModal(false);
+    reset()
   }
 
-  const postOneProduct = event => {
-    const ref = document.getElementById('ref').value;
-    const name = document.getElementById('name').value;
-    const cost = document.getElementById('cost').value;
-    const price = document.getElementById('price').value;
-    const image = document.getElementById('image').value;
+  const postOneProduct = (data) => {
+    // setPosting(false);
+    console.log('Errorsitos: ',errors);
 
-    if (ref === '' || name === '' || cost === '' || price === '' || image === '') {
-      alert('Complete la información');
-    }else{
-      fetch('http://localhost:3002/api/v1/products', {
-        method: 'POST',
-        body: JSON.stringify({
-            ref: ref,
-            name: name,
-            cost: cost,
-            price: price,
-            image: image
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        // Handle data
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-      
-      setTimeout(() => {
-        fetch("http://localhost:3002/api/v1/products")
-            .then((response) => response.json())
-            .then((data) => {
-              // setProducts(data); // ⬅️ Guardar datos
-              setProductsName(data)
-              console.log('products array desde modal: ',productsNameArray); 
-            });
-            setOpenModal(false);
-      }, 1000);
+    if (JSON.stringify(errors) === '{}') {
+      setPosting(true);
     }
 
+    fetch('http://localhost:3002/api/v1/products', {
+      method: 'POST',
+      body: JSON.stringify({
+          ref: data.ref,
+          name: data.name,
+          cost: parseInt(data.cost),
+          price:  parseInt(data.price),
+          image: data.image
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+    
+    setTimeout(() => {
+      fetch("http://localhost:3002/api/v1/products")
+          .then((response) => response.json())
+          .then((data) => {
+            setProductsName(data); 
+          });
+          setOpenModal(false);
+          // setPosting(true);
+          setPosting(false);
+    }, 2000);
+    reset();
   }
 
   return (
@@ -65,30 +68,47 @@ function ModalPostProduct({
               <h2>Agregar un producto</h2>
               <button onClick={closeModal}> <MdCancel /></button>
             </div>
-            <form action="" className='form-container'>
-              <div>
-                <label htmlFor="">Referencia</label>
-                <input type="text" placeholder='Referencia' id='ref' required/>
-              </div>
-              <div>
-                <label htmlFor="">Nombre</label>
-                <input type="text" placeholder='Nombre' id='name' required/>
-              </div>
-              <div>
-                <label htmlFor="">Costo</label>
-                <input type="text" placeholder='Costo' id='cost' required/>
-              </div>
-              <div>
-                <label htmlFor="">Precio</label>
-                <input type="text" placeholder='Precio' id='price' required/>
-              </div>
-              <div>
-                <label htmlFor="">Imagen</label>
-                <input type="text" placeholder='Imagen' id='image' required/>
-              </div>
-            </form>
+            {!posting ?
+                <form id="product-form" onSubmit={handleSubmit(postOneProduct)} className='form-container'>
+                  <div>
+                    <label htmlFor="">Referencia</label>
+                    <input type="text" placeholder='Referencia' {...register('ref', {
+                      required: true
+                    })} />
+                    {/* {errors.ref?.type === 'required' && <p>Debe llenar el campo</p> */}
+                  </div>
+                  <div>
+                    <label htmlFor="">Nombre</label>
+                    <input type="text" placeholder='Nombre' {...register('name', {
+                      required: true
+                    })}  />
+                  </div>
+                  <div>
+                    <label htmlFor="">Costo</label>
+                    <input type="number" placeholder='Costo' {...register('cost', {
+                      required: true
+                    })} />
+                  </div>
+                  <div>
+                    <label htmlFor="">Precio</label>
+                    <input type="number" placeholder='Precio' {...register('price', {
+                      required: true
+                    })} />
+                  </div>
+                  <div>
+                    <label htmlFor="">Imagen</label>
+                    <input type="text" placeholder='Imagen' {...register('image', {
+                      required: true
+                    })} />
+                  </div>
+                </form>
+              :
+                <div className='loading-post'>
+                  <div className='spinner'></div>
+                </div>
+            }
             <div className='modal-footer'>
-              <button onClick={postOneProduct} className='save-modal-btn'><span>Guardar</span><RiSave3Fill /></button>
+              <button type="submit" form="product-form" className='save-modal-btn'><span>Guardar</span><RiSave3Fill /></button>
               <button onClick={closeModal} className='cancel-modal-btn'><span>Cancelar</span><MdCancel /></button>
             </div>
           </div>
